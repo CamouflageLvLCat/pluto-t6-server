c963a9828868736b9ff800b0682203e2bcba69d1
LTDA-lab <111789082+CamouflageLvLCat@users.noreply.github.com>
2026-02-04 07:57:03 -0300

    Update fix-permissions.sh

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find /home/ubuntu/t6server-public -type d -exec chmod 755 {} \;
-find /home/ubuntu/t6server-public -type f -exec chmod 644 {} \;
-find /home/ubuntu/t6server-public -type f -name "*.sh" -exec chmod 755 {} \;
-find /home/ubuntu/t6server-public -type f -name "*.bat" -exec chmod 755 {} \;
+find /home/ubuntu/pluto-t6-server -type d -exec chmod 755 {} \;
+find /home/ubuntu/pluto-t6-server -type f -exec chmod 644 {} \;
+find /home/ubuntu/pluto-t6-server -type f -name "*.sh" -exec chmod 755 {} \;
+find /home/ubuntu/pluto-t6-server -type f -name "*.bat" -exec chmod 755 {} \;
