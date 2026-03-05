4fcc699fdd9852ad37cb9734826f0f59f4d00f58
LTDA-lab <111789082+CamouflageLvLCat@users.noreply.github.com>
2026-03-05 02:55:23 -0300

    Update KillDeathMonitor.js

--- IW4MAdmin-2025.8.16.1/Plugins/KillDeathMonitor.js
+++ IW4MAdmin-2025.8.16.1/Plugins/KillDeathMonitor.js
@@ -1,6 +1,6 @@
 plugin = {
     author: "LTDA-lab",
-    version: 1.2,
+    version: 1.3,
     name: "KillDeathMonitor",
 
     onEventAsync: function (gameEvent, server) {
@@ -10,18 +10,21 @@ plugin = {
         var origin = gameEvent.Origin;
         var target = gameEvent.Target;
 
-        // FIX: permitir kills sin origin (scorestreaks)
         if (!target || !server.Clients.includes(target)) return;
 
         // --- Extraer posible arma ---
         var weapon = null;
+
         if (gameEvent.Data) {
-            if (typeof gameEvent.Data.Weapon === "string" && gameEvent.Data.Weapon.length) weapon = gameEvent.Data.Weapon;
-            else if (typeof gameEvent.Data.KillerWeapon === "string" && gameEvent.Data.KillerWeapon.length) weapon = gameEvent.Data.KillerWeapon;
+            if (typeof gameEvent.Data.Weapon === "string" && gameEvent.Data.Weapon.length)
+                weapon = gameEvent.Data.Weapon;
+            else if (typeof gameEvent.Data.KillerWeapon === "string" && gameEvent.Data.KillerWeapon.length)
+                weapon = gameEvent.Data.KillerWeapon;
             else {
                 for (var key in gameEvent.Data) {
                     if (!gameEvent.Data.hasOwnProperty(key)) continue;
                     var val = gameEvent.Data[key];
+
                     if (typeof val === "string" && /weapon|mp_|_mp/i.test(val)) {
                         weapon = val;
                         break;
@@ -29,20 +32,30 @@ plugin = {
                 }
             }
         }
+
         if (!weapon && typeof gameEvent.WeaponName === "string") weapon = gameEvent.WeaponName;
         if (!weapon && typeof gameEvent.Weapon === "string") weapon = gameEvent.Weapon;
 
-        // Limpiar weapon
+        // --- LIMPIAR NOMBRE DEL ARMA ---
         if (weapon) {
             weapon = String(weapon).trim().toLowerCase();
-            weapon = weapon.replace(/^(?:mp[_-]*)+/i, "");
-            weapon = weapon.replace(/(?:[_-]*mp)+$/i, "");
+
+            // obtener nombre base antes de _mp (elimina todos los accesorios)
+            var match = weapon.match(/^(.+?)_mp/);
+            if (match) {
+                weapon = match[1];
+            }
+
+            // limpiar underscores sobrantes
             weapon = weapon.replace(/^_+|_+$/g, "");
-            weapon = weapon.split(/[_-]/)[0];
         }
 
         // Armas especiales
-        var weaponMap = { "satchel": "c4", "7": "mp7" };
+        var weaponMap = {
+            "satchel": "c4",
+            "7": "mp7"
+        };
+
         if (weapon && weaponMap[weapon]) weapon = weaponMap[weapon];
 
         try {
@@ -54,20 +67,25 @@ plugin = {
                 origin.Tell("^2You ^7killed ^1" + target.Name);
                 target.Tell("^1" + origin.Name + " ^7killed ^2" + target.Name);
             }
+
             // --- Cuando te matan ---
             else if (target.Name === myName) {
                 if (origin) {
                     origin.Tell("^2" + origin.Name + " ^7" + (weapon ? "[" + weapon + "] " : "") + "^1" + target.Name);
                 }
+
                 target.Tell("^1" + originName + " ^7" + (weapon ? "[" + weapon + "] " : "") + "^2You");
             }
+
             // --- Otros entre sí ---
             else {
                 if (origin) {
                     origin.Tell("^2" + origin.Name + " ^7" + (weapon ? "[" + weapon + "] " : "") + "^1" + target.Name);
                 }
+
                 target.Tell("^1" + originName + " ^7" + (weapon ? "[" + weapon + "] " : "") + "^2" + target.Name);
             }
+
         } catch (ex) {
             if (server.Logger && typeof server.Logger.WriteWarning === "function") {
                 server.Logger.WriteWarning("Error al procesar evento de muerte: " + ex.message);
